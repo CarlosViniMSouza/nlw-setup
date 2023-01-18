5775d6f632b30b57b62a2b0197dc8e71b15df042
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2023-01-18 13:07:07 -0300

    lesson 02 -> done

---
+++ autogit.sh
@@ -0,0 +1,8 @@
+git init
+git add .
+git commit -m "lesson 02 -> done"
+git branch -M main
+git remote add origin https://github.com/CarlosViniMSouza/nlw-setup.git
+git push -u origin main
+git status
+
